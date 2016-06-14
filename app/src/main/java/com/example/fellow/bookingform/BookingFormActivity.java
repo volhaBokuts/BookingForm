e609623f2116a4750f4105db0721b1e05fa4e7ea
Fellow <bokut.olya@gmail.com>
2016-06-14 12:07:25 +0300

    Create repository

---
+++ app/src/main/java/com/example/fellow/bookingform/BookingFormActivity.java
@@ -0,0 +1,13 @@
+package com.example.fellow.bookingform;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BookingFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking_form);
+    }
+}
